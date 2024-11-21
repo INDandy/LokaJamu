25b90ceab3801a5baf5bb8260384892bae222be4
Abdul Hafizh Subhan <hafizhjr21@gmail.com>
2024-11-21 19:32:30 +0900

    membuat splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.play.services.maps)
     implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
